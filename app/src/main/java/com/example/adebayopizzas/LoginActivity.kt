aa94e1af751a365bdfebdc1fd74a4d6fcdf01e9a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-28 11:14:37 -0300

    Função de logar na tela de login

--- app/src/main/java/com/example/adebayopizzas/LoginActivity.kt
+++ app/src/main/java/com/example/adebayopizzas/LoginActivity.kt
@@ -1,20 +1,41 @@
 package com.example.adebayopizzas
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.adebayopizzas.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun logar(view: View) {
+        //Recuperar os valores digitados
+        val login = binding.etLogin.text.toString()
+        val senha = binding.etSenha.text.toString()
+        //Valida o usuário e senha
+        if (login == "FIAP" && senha == "123") {
+            //Mudar de tela Tela de Destino
+            val intent = Intent(this, MainActivity::class.java)
+            //Passar uma valor para a outra tela
+            intent.putExtra("usuario", login)
+            //Iniciar a outra tela
+            startActivity(intent)
+            finish()
+        } else {
+            //Login ou senha inválidos
+            Toast.makeText(this, "Usuário ou senha inválidos",
+                Toast.LENGTH_SHORT).show()
         }
     }
+
 }
